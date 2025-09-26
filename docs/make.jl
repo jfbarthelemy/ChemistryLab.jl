174211b7b182d14f4990aca01ee3b2012fadf124
anthonysoive <anthony.soive@cerema.fr>
2025-09-26 18:07:02 +0200

    Refactor documentation setup by commenting out optional parameters in makedocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,5 @@
+push!(LOAD_PATH,"../src/")
+
 using CementChemistry
 using Documenter
 
@@ -5,16 +7,16 @@ DocMeta.setdocmeta!(CementChemistry, :DocTestSetup, :(using CementChemistry); re
 
 makedocs(;
     modules=[CementChemistry],
-    doctest = true,
-    linkcheck = true,
-    authors="Jean-François Barthélémy and Anthony Soive",
-    repo = "https://github.com/jfbarthelemy/CementChemistry.jl/blob/{commit}{path}#{line}",
+    # doctest = true,
+    # linkcheck = true,
+    # authors="Jean-François Barthélémy and Anthony Soive",
+    # repo = "https://github.com/jfbarthelemy/CementChemistry.jl/blob/{commit}{path}#{line}",
     sitename="CementChemistry.jl",
     format=Documenter.HTML(;
         prettyurls = true,#get(ENV, "CI", "false") == "true",
-        canonical="https://jfbarthelemy.github.io/CementChemistry.jl",
+        # canonical="https://jfbarthelemy.github.io/CementChemistry.jl",
         #edit_link="main",
-        assets = ["assets/style.css"],
+        # assets = ["assets/style.css"],
         ),
     pages=[
         "Home" => "index.md",
